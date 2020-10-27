cf20dbf37ea1f0295aab7ccf7bb2162516a5206d
Enrico Borges <enricolbc@al.insper.edu.br>
2020-10-27 12:06:37 -0300

    dcc

---
+++ pecas.py
@@ -0,0 +1,6 @@
+import scaner
+class piao:
+    def __init__(self,cor,inicial):
+        self.cor=cor
+        self.inicialx=scaner(inicial)[0]
+        self.inicialy=scaner(inicial)[1]
---
+++ scaner.py
@@ -0,0 +1,3 @@
+def scaner(posicao):
+    
+
