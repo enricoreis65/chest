c60d22ef37c7c2367e6e32f4fde6313c53034fef
Enrico Borges <enricolbc@al.insper.edu.br>
2020-10-27 21:43:09 -0300

    movimeto piao 0k

--- mapa.py
+++ mapa.py
@@ -15,18 +15,26 @@ class tab:
         
     def atualiza_tabuleiro(self,pos):
         if len(pos)==4:
-            
-            if dicionario_pos[pos[0:2]][0].verifica_mov(pos)==True:
-                self.ta[scaner(pos)[0]][scaner(pos)[1]]="  "
-                self.ta[scaner(pos)[2]][scaner(pos)[3]]=dicionario_pos[pos[0:2]][1]
-            
-                dicionario_pos[pos[2:4]]=dicionario_pos[pos[0:2]]
-                dicionario_pos[pos[0:2]]='  '
-                self.printar_tabuleiro()
+            if pos[0:2] in dicionario_pos:
+                if dicionario_pos[pos[0:2]][0].verifica_mov(pos)==True:
+                    self.ta[scaner(pos)[0]][scaner(pos)[1]]="  "
+                    self.ta[scaner(pos)[2]][scaner(pos)[3]]=dicionario_pos[pos[0:2]][1]
+                
+                    dicionario_pos[pos[2:4]]=dicionario_pos[pos[0:2]]
+                    dicionario_pos[pos[0:2]]=""  ""
+                    self.printar_tabuleiro()
+                else:
+                    self.printar_tabuleiro()
             else:
-                self.printar_tabuleiro()
+
+                 print("esse movimento é ilegal")
+                 self.printar_tabuleiro()
         if len(pos)==2:
-            self.ta[scaner(pos)[0]][scaner(pos)[1]]=dicionario_pos[pos[0:2]][1]
+            if pos[0:2] in dicionario_pos:
+                self.ta[scaner(pos)[0]][scaner(pos)[1]]=dicionario_pos[pos[0:2]][1]
+            else:
+                print("esse movimento é ilegal")
+                self.printar_tabuleiro()
         print(dicionario_pos)
 class piao():
     def __init__(self,cor,inicial):
@@ -39,7 +47,7 @@ class piao():
         tabuleiro.atualiza_tabuleiro(posicao)
     def verifica_mov(self,pos):
         print(pos[0:2])
-        if pos[0:2] not in dicionario_pos and dicionario_pos[pos[0:2]]!="  ":
+        if dicionario_pos[pos[0:2]]=="  ":
             print("esse movimento é ilegal")
             return False
         else:
